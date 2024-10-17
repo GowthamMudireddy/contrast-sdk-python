7226b86b090f35d94dfb5009bc352d538feb32c3
GowthamMudireddy <90867512+GowthamMudireddy@users.noreply.github.com>
2024-10-16 20:14:47 -0400

    Update session_metadata_api.py

--- contrast_security/user_api/session_metadata_api.py
+++ contrast_security/user_api/session_metadata_api.py
@@ -33,3 +33,15 @@ class _SessionMetadataApi(_ApiSupport):
     def get_app_session_metadata_sessionid(self, org_uuid, app_id, session_id):
         path = 'organizations/{org_uuid}/applications/{app_id}/agent-sessions/{session_id}'.format(org_uuid=org_uuid, app_id=app_id, session_id=session_id)
         return self._get(path)
+
+    def filter_app_SbavRouteSession(self, org_uuid, app_id, session_id, metadata, limit=None, offset=None):
+        path = 'organizations/{org_uuid}/applications/{app_id}/agent-sessions/filter'.format(org_uuid=org_uuid, app_id=app_id,  session_id=session_id, metadata=metadata)
+
+        # Prepare query parameters
+        params = {}
+        if limit is not None:
+            params['limit'] = limit
+        if offset is not None:
+            params['offset'] = offset
+
+        return self._post(path, params=params)
